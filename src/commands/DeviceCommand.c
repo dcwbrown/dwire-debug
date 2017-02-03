b79094eaf1cb2c02c4868483eeb67e92a247ae22
David Brown <dave.github@brownsmeet.com>
2017-02-03 11:20:23 +0000

    Allow port spec as number only. Tidy port hunting messages.

--- src/commands/DeviceCommand.c
+++ src/commands/DeviceCommand.c
@@ -3,7 +3,15 @@
 void DeviceCommand() {
   if (SerialPort) {Close(SerialPort);}
   Sb();
-  Ran(ArrayAddressAndLength(UsbSerialPortName));
+  if (IsNumeric(NextCh())) { // Just the port number is being specified
+    #ifdef windows
+      strncpy(UsbSerialPortName, "COM", 3); Rn(UsbSerialPortName+3, sizeof(UsbSerialPortName-3));
+    #else
+      strncpy(UsbSerialPortName, "ttyUSB", 6); Rn(UsbSerialPortName+6, sizeof(UsbSerialPortName-6));
+    #endif
+  } else {// The full port name is being specified
+    Ran(ArrayAddressAndLength(UsbSerialPortName));
+  }
   Sb();
   int baud;
   if (!IsDwEoln(NextCh())) {baud = ReadNumber(0);} else {baud = 0;}
